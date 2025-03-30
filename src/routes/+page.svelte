97f341d1a7d53c57b6f9210de2c404e87bdc0393
Julian4806 <210549@student.glu.nl>
2025-03-30 14:05:46 +0200

    conditionals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,61 @@
 <script lang="ts">
   import Header from "./Header.svelte";
-  let name = $state("Scott");
-  let status: "OPEN" | "CLOSED" = $state("OPEN");
-  let full_name = $derived(`${name} Tolinski`);
-  /*
-  function toggle() {
-    status = status === "OPEN" ? "CLOSED" : "OPEN";
-  }
-  */
-  function onclick() {
-    status = status === "OPEN" ? "CLOSED" : "OPEN";
-  }
+
+  let formState = $state({
+    name: "",
+    birthday: "",
+    step: 0,
+    error: "",
+  });
 </script>
 
-<Header {name} fake_name="Wess" />
+<main>
+  <p>Step: {formState.step + 1}</p>
 
-<h2>{full_name}</h2>
+  {#if formState.error}
+    <p class="error">
+      {formState.error}
+    </p>
+  {/if}
 
-<input type="text" bind:value={name} />
-
-<p>
-  The store is now {status}
-</p>
-<button {onclick}>Toggle Status</button>
-<hr />
-<button
-  onclick={() => {
-    status = status === "OPEN" ? "CLOSED" : "OPEN";
-  }}>Toggle Status inline-function</button
->
+  {#if formState.step === 0}
+    <div>
+      <label for="name">Your Name</label>
+      <input type="text" id="name" bind:value={formState.name} />
+    </div>
+    <button
+      onclick={() => {
+        if (formState.name !== "") {
+          formState.step++;
+          formState.error = "";
+        } else {
+          formState.error = "Your name is empty. Please write your name.";
+        }
+      }}>Next</button
+    >
+  {:else if formState.step === 1}
+    <div>
+      <label for="birthday">Your birthday</label>
+      <input type="date" id="birthday" bind:value={formState.birthday} />
+    </div>
+    <button
+      onclick={() => {
+        if (formState.birthday !== "") {
+          formState.step++;
+          formState.error = "";
+        } else {
+          formState.error =
+            "Your birthday is empty. Please write your birthday.";
+        }
+      }}>Next</button
+    >
+  {/if}
+</main>
 
 <style>
+  .error {
+    color: #ff0000;
+  }
   button {
     cursor: pointer;
     user-select: none;
