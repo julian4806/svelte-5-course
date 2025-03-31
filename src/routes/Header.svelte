18307e6a1c0b9e6cb9ac1d0179157bc0e9f41e9f
Julian4806 <210549@student.glu.nl>
2025-03-31 16:30:16 +0200

    complex states

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
+  import { createState } from "./state.svelte";
   import type { Snippet } from "svelte";
   import { render } from "svelte/server";
 
   let { name }: { name: string } = $props();
+
+  const myState = createState();
 </script>
 
 <div class="header">
   <h1>
     {name ? name + "'s" : "..."} Form
   </h1>
+
+  <button onclick={myState.up}>{myState.value}</button>
+  <input bind:value={myState.value} />
 </div>
 
 <style>
